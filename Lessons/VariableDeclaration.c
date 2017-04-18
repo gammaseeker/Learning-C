b84b304f11835400d8a722ce933ff0eac285db73
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-04-18 14:48:50 -0400

    Create VariableDeclaration.c

---
+++ Lessons/VariableDeclaration.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() 
+{
+    int x;
+    printf( "Declare x first" );
+
+   
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+    /* wrong!  The variable declaration must appear first */
+    printf( "Declare x next" );
+    int x;
+
+   
+}
+*/
