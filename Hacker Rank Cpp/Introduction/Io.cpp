f45beae4cff9561041609d3cecb237c1b7442a6e
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-09-03 09:37:25 -0400

    Create Io.cpp

---
+++ Hacker Rank Cpp/Introduction/Io.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n1 = 0, n2 = 0, n3 = 0;
+    std::cin >> n1 >> n2 >> n3;
+    int sum = n1 + n2 + n3;
+    std::cout << sum;
+    return 0;
+}
