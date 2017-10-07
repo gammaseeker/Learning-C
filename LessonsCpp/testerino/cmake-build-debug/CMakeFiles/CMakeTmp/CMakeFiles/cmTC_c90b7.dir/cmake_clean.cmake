adecbd7353a583b63b98c74e81c54da768898370
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-10-06 20:56:54 -0400

    clion works!!!

---
+++ LessonsCpp/testerino/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_c90b7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_c90b7.dir/feature_tests.cxx.o"
+  "cmTC_c90b7.pdb"
+  "cmTC_c90b7.exe"
+  "libcmTC_c90b7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_c90b7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LessonsCpp/testerino/cmake-build-debug/CMakeFiles/testerino.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testerino.dir/main.cpp.o"
+  "testerino.pdb"
+  "testerino.exe"
+  "libtesterino.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testerino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
