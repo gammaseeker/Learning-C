05c05cf2f5cde2864cbdc37baa80405390b64cf9
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-10-08 21:52:59 -0400

    framing

--- AcceleratedCpp/Chp1.cpp
+++ AcceleratedCpp/Chp1.cpp
@@ -14,10 +14,28 @@ void printmessage()
   cout << "I'm a function!";
 }
 
+void frameName()
+{
+	cout << "Please enter your first name: ";
+	string name;
+	cin >> name;
+	const string greeting = "Hello, " + name + "!";
+	const string spaces(greeting.size(), ' ');
+	const string second = "* " + spaces + " *";
+	const string first(second.size(), '*');
+	cout << endl;
+	cout << first << endl;
+	cout << second << endl;
+	cout << " " << greeting << " *" << endl;
+	cout << second << endl;
+	cout << first << endl;
+}
+
 int main()
 {
 	//Order matters. Functions must come before main method
 	input();
 	printmessage();
+	frameName();
 	return 0;
 }
