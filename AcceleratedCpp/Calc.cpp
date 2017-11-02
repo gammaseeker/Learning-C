38017bf931b1a93efc68a6163c843985eee0b5a0
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2017-11-02 16:27:53 -0400

    Add files via upload

--- AcceleratedCpp/Calc.cpp
+++ AcceleratedCpp/Calc.cpp
@@ -2,18 +2,31 @@
 
 using namespace std;
 
+void printSomething(int num)
+{
+	cout << "The number you have given me is " << num << endl;
+}
+
+int sum(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	int a;
 	int b;
 	int sum;
 
-	cout << "Enter a number boss! \n";
+	cout << "Enter a number \n";
 	cin >> a;
 	cout << "Enter another number \n";
 	cin >> b;
-	sum = a + b;
-	cout << "The sum is: " << sum;
+	sum = sum(a, b);
+	cout << "The sum is: " << sum << endl;
+	//cout << "The sum is: " << sum(a, b) << endl; This is an eclipse bug, it should run
+
+	printSomething(sum);
 
 	return 0;
 }
