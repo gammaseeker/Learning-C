78ad87ad81e0d010d0f1c3c775d5dac3e450790d
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-02-03 17:43:38 -0500

    commented some notes

--- EclipseCWorkspace/FirstProject/helloWorld.c
+++ EclipseCWorkspace/FirstProject/helloWorld.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> //preprocessor command, tells compiler to include contents of standard input and output
 int main()
 {
 	printf("Hello, World!");
-	return 0;
+	return 0;// exit status
 }
