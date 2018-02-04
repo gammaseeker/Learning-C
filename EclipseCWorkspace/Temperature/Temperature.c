19a653b1fd38bd5c41866cab2063d3c5a8f877cf
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-02-03 20:21:46 -0500

    floating point

--- EclipseCWorkspace/Temperature/Temperature.c
+++ EclipseCWorkspace/Temperature/Temperature.c
@@ -2,7 +2,7 @@
 
 main()
 {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 	lower = 0;
 	upper = 300;
@@ -11,7 +11,7 @@ main()
 	fahr = lower;
 	while(fahr <= upper){
 		celsius = 5 * (fahr-32) / 9;
-		printf("%d\t%d\n", fahr, celsius);
+		printf("%3.0f %6.2f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
