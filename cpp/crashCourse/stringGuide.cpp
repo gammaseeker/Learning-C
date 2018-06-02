a296d08418759ce20f093b4ddf09812571f37776
Joey Jiemjitpolchai <jjiemjitpolchai9540@bths.edu>
2018-06-02 17:04:49 -0400

    Add files via upload

--- cpp/crashCourse/stringGuide.cpp
+++ cpp/crashCourse/stringGuide.cpp
@@ -14,5 +14,16 @@ int main(){
     cout << dog.compare(cat) << endl;
     cout << dog.compare(dog) << endl;
     cout << cat.compare(dog) << endl;
+
+    string name;
+    cout << "What is your name ";
+    getline(cin, name);
+    cout << "Hello " << name << endl;
+    string wholeName = name.assign(name);
+    cout << wholeName << endl;
+    string firstName = wholeName.assign(wholeName, 0, 4);
+    cout << firstName << endl;
+    int lastNameIndex = name.find("cat", 0);
+    
     return 0;
 }
